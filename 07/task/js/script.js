a1c91aeea34e9ddbf356dc971c6d4626bda5c863
Vlad Savluk <priora228lol@icloud.com>
2018-07-04 16:05:19 +0300

    first task

---
+++ 07/task/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let a = 3;
+let b = 4;
+let x ;
+if (a > b) {
+    x = a + b / 2 * 4;
+}
+else if (a === b) {
+    x = 400;
+}
+else if (a < b) {
+    x = a - b + 2 / b * 4;
+}
+
+console.log(x);
