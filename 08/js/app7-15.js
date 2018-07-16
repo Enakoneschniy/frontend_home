1c49a9742cdb50797560522ff31bc739ec521014
Serhii_Dolhopolov <arhangelnight@gmail.com>
2018-07-16 20:09:50 +0300

    homework-8

---
+++ 08/js/app7-15.js
@@ -0,0 +1,42 @@
+
+for (let x = 4; x <= 400; x++) {
+    console.log(x);
+}
+
+for (let y = 4; y < 14; y + 3) {
+    console.log(y);
+}
+
+for (let z = 654; z > 0; z--) {
+    console.log(z);
+}
+
+for (let i = 1983; i < 2017; i++) {
+    console.log(i + 'год');
+}
+
+for (let p = -4; p < 100; p += 2) {
+    console.log(p);
+}
+
+for (let m = 1; m < 9; m++) {
+    let w = m * 7;
+    console.log(w);
+}
+
+for (let k = 1000; k < 2000; k++) {
+    console.log(String.fromCharCode(k));
+}
+
+let t = 0;
+for (let n = 0; n <= 100; n++) {
+    t = t + n;
+    console.log(t)
+}
+
+let d = 1;
+for (let h = 1; h <= 50; h++) {
+    d *= h;
+    console.log(d)
+}
+
